b49872e2e2e80201c93bc1ec819e2cd14a25ebe3
Tong Qi <tongqii@163.com>
2015-01-08 00:01:15 +0800

    add tag_blogs

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.BlogIndex.as_view(), name="blog"),
     url(r'^category/(?P<slug>\S+)/$', views.BlogIndex.as_view(), name="category_blogs"),
+    url(r'^tag/(?P<pk>\S+)/$', views.BlogIndex.as_view(), name="tag_blogs"),
     url(r'^(?P<slug>\S+)$', views.BlogDetail.as_view(), name="blog_detail"),
 )
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,8 @@ __author__ = 'chengbin.wang'
 from django.views import generic
 from . import models
 
+categories_tags = {"categories": models.Category.objects.all(), "tags": models.Tag.objects.all()}
+
 
 class ExtraContext(object):
     extra_context = {}
@@ -19,20 +21,23 @@ class ExtraContext(object):
 
 
 class BlogIndex(ExtraContext, generic.ListView):
-    extra_context = {"categories": models.Category.objects.all()}
+    extra_context = categories_tags
     template_name = "blogs.html"
     paginate_by = 5
 
     def get_queryset(self):
         slug = self.kwargs.get('slug', None)
+        pk = self.kwargs.get('pk', None)
         if slug:
             return models.Blog.objects.published().filter(category__slug=slug)
+        elif pk:
+            return models.Tag.objects.get(pk=pk).blog_set.all()
         else:
             return models.Blog.objects.published()
 
 
 class BlogDetail(ExtraContext, generic.DetailView):
-    extra_context = {"categories": models.Category.objects.all()}
+    extra_context = categories_tags
     model = models.Blog
     template_name = "post.html"
 
